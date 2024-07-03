e01505cf29f78f2b18be8acceadd16902e480f13
mafiapatch <g8071107@gmail.com>
2024-07-03 16:40:59 +0800

    [modify] update liff url

--- src/views/DetailView.vue
+++ src/views/DetailView.vue
@@ -110,7 +110,7 @@ export default {
         </div>
 
         <div class="col-12 px-5 BarDetailAction">
-          <a class="getTicket" href="https://inspire-your-new.accuhit.com.tw/member/giftbox?key=a24a844769eaa77cd43b2f01ca851e7f" target="_blank"><img src="/assets/img/get_ticket.png" /></a>
+          <a class="getTicket" href="https://liff.line.me/1657256835-VrMKDJ6W/member/giftbox?key=a24a844769eaa77cd43b2f01ca851e7f" target="_blank"><img src="/assets/img/get_ticket.png" /></a>
           <a class="previous" @click="toPrevious()"><img src="/assets/img/previous_member.png" /></a>
         </div>
       </div>
