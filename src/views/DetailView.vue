c98498be3914faf372fdb50f0b16b4fa168a6322
mafiapatch <g8071107@gmail.com>
2024-09-13 12:31:18 +0800

    [modify] change image

--- src/views/DetailView.vue
+++ src/views/DetailView.vue
@@ -110,7 +110,7 @@ export default {
         </div>
 
         <div class="col-12 px-5 BarDetailAction">
-          <a class="getTicket" href="https://liff.line.me/1657256835-VrMKDJ6W/member/giftbox?key=cac32718c666e9f3050a6e830700e23d" target="_blank"><img src="/assets/img/get_ticket.png" /></a>
+          <a class="getTicket" href="https://liff.line.me/1657256835-VrMKDJ6W/member/giftbox?key=290e52f5bb1bc357894fb27158cd4a40" target="_blank"><img src="/assets/img/get_ticket.png" /></a>
           <a class="previous" @click="toPrevious()"><img src="/assets/img/previous_member.png" /></a>
         </div>
       </div>
